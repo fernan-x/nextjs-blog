8fe412f6c4de10630299865e58d02a60db6fb7f7
Fernandes Alves Fabien <fabien@code42.fr>
2022-02-25 07:41:09 +0100

    Added : Post page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import Header from '../components/Header'
-import { sanityClient } from '../lib/sanity'
+import { sanityClient, urlFor } from '../lib/sanity'
 import { Post } from '../types/common/typings'
 
 interface Props {
@@ -32,8 +33,6 @@ export const getServerSideProps = async () => {
 }
 
 const Home: NextPage<Props> = ({ posts }: Props) => {
-  console.log(posts)
-
   return (
     <div className="mx-auto max-w-7xl">
       <Head>
@@ -65,6 +64,32 @@ const Home: NextPage<Props> = ({ posts }: Props) => {
       </div>
 
       {/* Posts */}
+      <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="group cursor-pointer overflow-hidden rounded-lg border">
+              <img
+                className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
+                src={urlFor(post.mainImage).url()!}
+                alt="Post preview"
+              />
+              <div className="flex justify-between bg-white p-5">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt="Profile"
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
---
+++ pages/post/[slug].tsx
@@ -0,0 +1,127 @@
+import { GetStaticProps, NextPage } from 'next'
+import PortableText from 'react-portable-text'
+import Header from '../../components/Header'
+import { sanityClient, urlFor } from '../../lib/sanity'
+import { Post } from '../../types/common/typings'
+
+interface Props {
+  post: Post
+}
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post"] {
+          _id,
+          slug {
+              current
+          }
+      }`
+
+  const posts = await sanityClient.fetch(query)
+
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }))
+
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "post" && slug.current == $slug][0] {
+      _id,
+      _createdAt,
+      title,
+      author -> {
+        name, 
+        image
+      },
+      'comments': *[
+        _type == "comment" &&
+        post._ref == ^._id &&
+        approved == true],
+      description,
+      mainImage,
+      slug,
+      body
+    }`
+
+  const post = await sanityClient.fetch(query, {
+    slug: params!.slug,
+  })
+
+  if (!post) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60, // Refresh the old cache every 60 seconds
+  }
+}
+
+const Post: NextPage<Props> = ({ post }: Props) => {
+  return (
+    <main>
+      <Header />
+
+      <img
+        className="h-40 w-full object-cover"
+        src={urlFor(post.mainImage).url()}
+        alt="Cover"
+      />
+
+      <article className="mx-auto max-w-3xl p-5">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+        <h2 className="mb-2 text-xl font-light text-gray-500">
+          {post.description}
+        </h2>
+
+        <div className="flex items-center space-x-2">
+          <img
+            className="h-10 w-10 rounded-full"
+            src={urlFor(post.author.image).url()}
+            alt="User"
+          />
+          <p className="text-sm font-extralight">
+            Blog post by{' '}
+            <span className="text-green-600">{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+
+        <div className="mt-10">
+          <PortableText
+            className=""
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="my-5 text-2xl font-bold" {...props} />
+              ),
+              h2: (props: any) => (
+                <h2 className="my-5 text-xl font-bold" {...props} />
+              ),
+              li: (props: any) => <li className="ml-4 list-disc" {...props} />,
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
+          />
+        </div>
+      </article>
+    </main>
+  )
+}
+
+export default Post
